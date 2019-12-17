6cd8c453d42c1e42936baee6c9ebfbffeb5d1a92
rubensb-git <rubens.bento@gmail.com>
2019-12-16 21:22:28 -0300

    Exception handling - delete

--- src/main/java/com/educandoweb/course1/services/UserService.java
+++ src/main/java/com/educandoweb/course1/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.course1.entities.User;
 import com.educandoweb.course1.repositories.UserRepository;
+import com.educandoweb.course1.services.exceptions.DatabaseException;
 import com.educandoweb.course1.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,16 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id); 
+		}
+		
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage()); 
+		}
 	}
 	
 	public User update(Long id, User obj) {
---
+++ src/main/java/com/educandoweb/course1/services/exceptions/DatabaseException.java
@@ -0,0 +1,10 @@
+package com.educandoweb.course1.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+
+}
